3bb005d9105e68eb051c7bd4c52ca5e210a275a7
Jay002 <jayesh2598@gmail.com>
2020-10-03 12:14:09 +0530

    Tested: 'I am in any mood'returns 'happy'

--- test/java/com/capgemini/learningTest/MoodAnalyserTest.java
+++ test/java/com/capgemini/learningTest/MoodAnalyserTest.java
@@ -9,10 +9,18 @@ import com.capgemini.learning.MoodAnalyser;
 public class MoodAnalyserTest {
 	
 	@Test
-	public void testingMood() {
+	public void testingSadMood() {
 		MoodAnalyser mood = new MoodAnalyser();
 		String expectedMood= "Sad";
 		String actualMood= mood.analyseMood("I am in sad mood.");
+		assertEquals(expectedMood, actualMood);
+	}
+	
+	@Test
+	public void testingAnyMood() {
+		MoodAnalyser mood = new MoodAnalyser();
+		String expectedMood= "Happy";
+		String actualMood= mood.analyseMood("I am in any mood.");
 		assertEquals(expectedMood, actualMood);
 	}
 }
